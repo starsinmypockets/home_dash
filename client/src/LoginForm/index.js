c3f25ff6e9bcb75bccdbcb668744bc3f034f38f8
Paul <starsinmypockets@gmail.com>
2021-03-13 19:18:51 +0000

    [client][s] Style login page

--- client/src/LoginForm/index.js
+++ client/src/LoginForm/index.js
@@ -10,31 +10,29 @@ const LoginForm = props => {
 
   
   return (
-  <div className="text-left w-1/4 mt-12 mx-auto">
+  <div>
     { !showLogin && 
-      <button onClick={_ => logoutSubmit()} className="flex-1 bg-blue-500 text-white text-center font-bold w-20 p-2 m-2 rounded">Log out</button>
+      <span onClick={_ => logoutSubmit()} className="absolute top-0 right-0 font-bold w-20 p-2 m-2 cursor-pointer">Log out</span>
     }
     { showLogin &&
-    <div>
-      <p>Please log in to visit your dashboard</p>
-      <div>
-        <div>
-          <label for="username" className="mr-2">Username</label>
-          <input type="text" name="username" placeholder="Username" onChange={e => {
+    <div className="mt-24">
+      <p className="text-2xl">Please log in to visit your dashboard</p>
+      <div className="w-1/4 mx-auto bg-gray-200 p-12 rounded-2xl">
+        <div className="mb-2">
+          <input className="p-2 rounded" type="text" name="username" placeholder="Username" onChange={e => {
               setUsername(e.target.value) 
             }
           }
           />
         </div>
         <div>
-          <label for="password" className="mr-2">Password</label>
-          <input type="passwrd" placeholder="Password" onChange={e => {
+          <input className="p-2 rounded" type="password" placeholder="Password" onChange={e => {
               setPassword(e.target.value)
             }
           }/>
         </div>
         <div>
-          <button onClick={_ => loginSubmit(username, password)} className="flex-1 bg-blue-500 text-white text-center font-bold w-20 p-2 m-2 rounded">Submit</button>
+          <button onClick={_ => loginSubmit(username, password)} className="flex-1 bg-blue-500 text-white text-center font-bold w-20 p-2 mt-2 rounded">Submit</button>
         </div>
       </div>
     </div>
